3b8228f3226917bb5f94a288c54e054c8ed9da1f
Jun Gan <jungan@Juns-MacBook-Pro.local>
2021-03-24 18:35:41 -0400

    Initialized project for XCode

---
+++ Restaurant-Guide-Swift-iOS/Restaurant-Guide-Swift-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant-Guide-Swift-iOS
+//
+//  Created by Jun Gan on 2021-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
