db7853edfb641858fcaeea46b3d04cbb9bc7b39c
Jun Gan <jungan@Juns-MacBook-Pro.local>
2021-03-25 20:39:05 -0400

    added textfield labels on login page

--- Restaurant-Guide-Swift-iOS/Restaurant-Guide-Swift-iOS/ViewController.swift
+++ Restaurant-Guide-Swift-iOS/Restaurant-Guide-Swift-iOS/ViewController.swift
@@ -8,16 +8,22 @@
 import UIKit
 import MaterialComponents
 import MaterialComponents.MaterialAppBar
+import MaterialComponents.MaterialTextControls_FilledTextAreas
+import MaterialComponents.MaterialTextControls_FilledTextFields
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var NameInput: MDCTextField!
-    var name: MDCTextInputControllerOutlined?
+    @IBOutlet weak var emailInput: MDCTextField!
+    var email: MDCTextInputControllerFilled?
+    
+    @IBOutlet weak var passwordInput: MDCTextField!
+    var password: MDCTextInputControllerFilled?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        name = MDCTextInputControllerOutlined(textInput: NameInput)
+        email = MDCTextInputControllerFilled(textInput: emailInput)
+        password = MDCTextInputControllerFilled(textInput: passwordInput)
     }
 }
 
