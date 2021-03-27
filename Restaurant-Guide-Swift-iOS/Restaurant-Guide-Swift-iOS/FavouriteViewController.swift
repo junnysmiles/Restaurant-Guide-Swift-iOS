0913c0e7ea3fcd720cf92e0d971a36ee57f61377
Jun Gan <jungan@Juns-MacBook-Pro.local>
2021-03-26 23:49:42 -0400

    updated some things

--- Restaurant-Guide-Swift-iOS/Restaurant-Guide-Swift-iOS/FavouriteViewController.swift
+++ Restaurant-Guide-Swift-iOS/Restaurant-Guide-Swift-iOS/FavouriteViewController.swift
@@ -1,8 +1,38 @@
 //
-//  FavouriteViewController.swift
+//  ViewController.swift
 //  Restaurant-Guide-Swift-iOS
 //
-//  Created by Jun Gan on 2021-03-25.
+//  Created by Jun Gan on 2021-03-24.
 //
 
 import Foundation
+import UIKit
+import MaterialComponents
+import MaterialComponents.MaterialBottomNavigation
+
+class FavouriteViewController: UIViewController {
+    
+    @IBOutlet weak var navBar: MDCBottomNavigationBar!
+    
+    @IBOutlet weak var favourites: UITabBarItem!
+    
+    @IBOutlet weak var add: UITabBarItem!
+    
+    @IBOutlet weak var location: UITabBarItem!
+    
+    @IBOutlet weak var about: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(navBar)
+        //navBar.titleVisibility = MDCBottomNavigationBarTitleVisibilitySelected
+        //navBar.alignment = MDCBottomNavigationBarAlignmentJustifiedAdjacentTiles
+        
+        favourites = UITabBarItem(
+            title: "Favourites",
+            image: UIImage(named:"ic_favourite"),
+            tag: 0)
+    }
+}
+
